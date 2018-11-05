272ae08de75f956ebf5fa278af840ac0a54ef1ca
Benjamin Hetherington <b.w.hetherington@gmail.com>
2018-11-05 21:31:49 +0100

    Add basic route

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,19 +9,22 @@ function notFoundResponse(path) {
 }
 
 const routes = {
-  '/all': server => async (req, res) => {
+  '/': _ => (_, res) => {
+    res.json({ error: 'not found' });
+  },
+  '/all': _ => async (req, res) => {
     logger.info(`all artifacts requested by ip: ${req.ip}`);
     const data = asyncIterator(queryGroups(groups));
     const received = await data.collect();
 
     res.json(received);
   },
-  '/sample': server => async (req, res) => {
+  '/sample': server => (req, res) => {
     logger.info(`daily sample requested by ip: ${req.ip}`);
     const sample = server.dailySample;
     res.json(sample);
   },
-  '/*': server => async (req, res) => {
+  '/*': _ => (req, res) => {
     logger.error(`unknown resource '${req.path}' requested by ip: ${req.ip}`);
     res.status = 404;
     res.json(notFoundResponse(res.path));
