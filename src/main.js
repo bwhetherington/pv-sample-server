6201625510265119f066523c29aff7a5fb8c0df7
Benjamin Hetherington <b.w.hetherington@gmail.com>
2018-12-05 11:56:03 +0100

    Fix previous commit

--- src/main.js
+++ src/main.js
@@ -1,41 +1,9 @@
 import Server from './server';
 import routes from './config/routes';
 
-import { asyncIterator, iterator } from 'lazy-iters';
-import fetch from 'node-fetch';
-
-import { writeFile } from 'fs';
-
-async function* queryAll() {
-  const url = 'http://localhost:8888/all';
-  const result = await fetch(url);
-  const data = await result.json();
-
-  yield* data;
-}
-
-async function run() {
-  const list = await asyncIterator(queryAll())
-    .map(artifact => ({
-      id: artifact.ck_id,
-      before: artifact.content.condition_in_1987,
-      after: artifact.content.condition_in_2000
-    }))
-    .filter(artifact => iterator(Object.values(artifact)).all(field => field !== undefined))
-    .map(({ id, before, after }) => ({
-      id,
-      before: parseInt(before[0]),
-      after: parseInt(after[0])
-    }))
-    .collect();
-  writeFile('conditions.json', JSON.stringify(list), console.log);
-}
-
 function main() {
-  run();
-
-  // const server = new Server(routes);
-  // server.start();
+  const server = new Server(routes);
+  server.start();
 }
 
 main();
